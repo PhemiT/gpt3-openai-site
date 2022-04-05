c91440c8f07d0238caf42980315f8cea7250c380
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-04-05 15:47:42 +0100

    Header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 Open AI</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur veritatis perferendis perspiciatis, aperiam aliquid iure quam quidem ipsa neque enim?</p>
+        <div className='gpt3__header-content__input'>
+          <input type="email" placeholder='Your Email Address' />
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt="people in the program" />
+          <p>1,600 people requested access in the last 24hours</p>
+        </div>
+
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
   )
 }
 
