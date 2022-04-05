27835d7534aae9f2e3d6c788d5d1b8e1fa44f93b
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-04-05 12:48:01 +0100

    Base Setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { Footer, Header, Features, Possibility, WhatGPT3, Blog } from './containers'
+import { CTA, Brand, NavBar } from './components'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient_bg'>
+        <NavBar />
+        <Header />
+      </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
