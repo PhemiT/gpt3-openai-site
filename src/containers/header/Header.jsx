e7ab3b1596d0ba7d049cb0a1cc82e3ce20a5f464
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-04-07 12:23:25 +0100

    Minor Edit

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className='gpt3__header section__padding' id='home'>
       <div className='gpt3__header-content'>
         <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 Open AI</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur veritatis perferendis perspiciatis, aperiam aliquid iure quam quidem ipsa neque enim?</p>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
         <div className='gpt3__header-content__input'>
           <input type="email" placeholder='Your Email Address' />
           <button type='button'>Get Started</button>
