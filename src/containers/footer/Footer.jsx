020a4e3b24f457a1b5bf976fa42fda35e9904ece
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-04-07 12:15:05 +0100

    Initial Final Commit: Base Complete

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      </div>
+
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="gpt3_logo" />
+          <p>Ikeja, Lagos, <br /> All Rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions </p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Ikeja, Lagos</p>
+          <p>+234 903 626 9576</p>
+          <p>phemiwrites@gmail.com</p>
+        </div>
+      </div>
+
+      <div className="gpt3__footer-copyright">
+        <p>@2021 GPT-3. All rights reserved.</p>
+      </div>
+  </div>
   )
 }
 
