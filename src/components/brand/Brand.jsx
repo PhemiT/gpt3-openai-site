b4639952eef40c1e23dc185cbd05d35dd61ab686
Bukunmi <phemiwrites@gmail.com>
2022-05-05 14:39:14 +0100

    Update Brand.jsx

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -12,9 +12,6 @@ const Brand = () => {
         <img src={slack} alt="slack" />
       </div>
       <div>
-        <img src={atlassian} alt="atlassian" />
-      </div>
-      <div>
         <img src={dropbox} alt="dropbox" />
       </div>
       <div>
